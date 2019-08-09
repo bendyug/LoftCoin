f995a29cd366cd21b40539ed094b0ac976dff6f2
bendyug <mitriy-b@mail.ru>
2019-08-09 12:48:27 +0300

    Added StrictMode and Timber for debugging

---
+++ app/src/main/java/com/dbendyug/loftcoin/log/DebugTree.java
@@ -0,0 +1,22 @@
+package com.dbendyug.loftcoin.log;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.Locale;
+
+import timber.log.Timber;
+
+@SuppressWarnings("ThrowableNotThrown")
+public class DebugTree extends Timber.DebugTree {
+
+    @Override
+    protected void log(int priority, String tag, @NotNull String message, Throwable t) {
+        final StackTraceElement ste = new Throwable().fillInStackTrace().getStackTrace()[5];
+        super.log(priority, tag, String.format(Locale.US, "[%s] %s(%s:%d): %s",
+                Thread.currentThread().getName(),
+                ste.getMethodName(),
+                ste.getFileName(),
+                ste.getLineNumber(),
+                message), t);
+    }
+}
